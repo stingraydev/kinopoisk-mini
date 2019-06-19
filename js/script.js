13e8b40b62b0bebbd2f5c4d04dc01e4e086e3720
OWIII <songlab.ru@gmail.com>
2019-06-20 00:01:44 +0300

    fixed a mistake

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ function apiSearch(event) {
     const searchText = document.querySelector(".form-control").value;
     const server = "https://api.themoviedb.org/3/search/multi?api_key=ad2b38c3623133c5e9dd093465546d14&language=ru&query=" + searchText;
     requestApi(server);
-};
+}
 
 searchForm.addEventListener("submit", apiSearch);
 
@@ -15,18 +15,17 @@ function requestApi(url) {
     request.open("GET", url);
     request.send();
     request.addEventListener("readystatechange", function() {
-    	if (request.readyState !== 4) return;
-    	if (request.status !== 200) {
-    		console.log("error:" + request.status);
-    		return;
-    	};
-    	const output = JSON.parse(request.responseText);
-    	let inner = "";
-    	output.results.forEach(function(item) {
-    		let nameItem = item.name || item.title;
-    		inner += '<div class="row col-lg-12">' + name.Item + '</div>';
-    	});
-    	movie.innerHTML = inner;
-		});
-};
-
+        if (request.readyState !== 4) return;
+        if (request.status !== 200) {
+            console.log("error:" + request.status);
+            return;
+        }
+        const output = JSON.parse(request.responseText);
+        let inner = "";
+        output.results.forEach(function(item) {
+            let nameItem = item.name || item.title;
+            inner += '<div class="col-12 col-md-4 col-xl-3">' + nameItem + '</div>';
+        });
+        movie.innerHTML = inner;
+    });
+}
