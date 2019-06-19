4a1784a49fb8e8240bb810633a57f2ea929e8643
Artem Lusnikov <songlab.ru@gmail.com>
2019-06-19 11:46:46 +0300

    Add scripts

--- js/script.js
+++ js/script.js
@@ -1,20 +1,32 @@
 const searchForm = document.querySelector("#search-form");
+const movie = document.querySelector("#movies");
 
 function apiSearch(event) {
     event.preventDefault();
-    const searchText = document.querySelector("#search-text").value;
-    const server = "https://api.themoviedb.org/3/search/multi?api_key=ad2b38c3623133c5e9dd093465546d14&language=ru&query=" + searhText;
-    requestApi(method, server);
-
+    const searchText = document.querySelector(".form-control").value;
+    const server = "https://api.themoviedb.org/3/search/multi?api_key=ad2b38c3623133c5e9dd093465546d14&language=ru&query=" + searchText;
+    requestApi(server);
 };
 
 searchForm.addEventListener("submit", apiSearch);
 
 function requestApi(url) {
     const request = new XMLHttpRequest();
-    console.log(request);
-    request.open(method, url);
+    request.open("GET", url);
     request.send();
-
-
+    request.addEventListener("readystatechange", function() {
+    	if (request.readyState !== 4) return;
+    	if (request.status !== 200) {
+    		console.log("error:" + request.status);
+    		return;
+    	};
+    	const output = JSON.parse(request.responseText);
+    	let inner = "";
+    	output.results.forEach(function(item) {
+    		let nameItem = item.name || item.title;
+    		inner += '<div class="row col-lg-12">' + name.Item + '</div>';
+    	});
+    	movie.innerHTML = inner;
+		});
 };
+
