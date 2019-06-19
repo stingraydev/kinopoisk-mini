dfbe1d71184dcc2213883db6d85f301c2e93454c
OWIII <songlab.ru@gmail.com>
2019-06-19 09:14:25 +0300

    +

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
+const searchForm = document.querySelector("#search-form");
 
+function apiSearch(event) {
+    event.preventDefault();
+    const searchText = document.querySelector("#search-text").value;
+    const server = "https://api.themoviedb.org/3/search/multi?api_key=ad2b38c3623133c5e9dd093465546d14&language=ru&query=" + searhText;
+    requestApi(method, server);
+
+};
+
+searchForm.addEventListener("submit", apiSearch);
+
+function requestApi(url) {
+    const request = new XMLHttpRequest();
+    console.log(request);
+    request.open(method, url);
+    request.send();
+
+
+};
